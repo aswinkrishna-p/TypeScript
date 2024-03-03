e6dd26e2529c21106dd94298a2a330d593fbaf33
aswin krishna <aswinkrishna33716@gmail.com>
2024-03-03 23:05:27 +0530

    ts basics

---
+++ js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let msg = 'first file in typescript';
+console.log(msg);
+let age = 20;
+if (age < 50)
+    age += 10;
+//# sourceMappingURL=main.js.map
---
+++ ts/main.ts
@@ -0,0 +1,6 @@
+let msg : string = 'first file in typescript'
+console.log(msg);
+
+let age : number = 20;
+if(age<50)
+age += 10
