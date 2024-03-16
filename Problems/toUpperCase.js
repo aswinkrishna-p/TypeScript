a957bfa4bb565bcd26eee9a45b0bca7bdd3e75d1
aswin krishna <aswinkrishna33716@gmail.com>
2024-03-16 23:54:05 +0530

    toUpperCase

---
+++ Problems/toUpperCase.js
@@ -0,0 +1,6 @@
+function capitalizeFirstLetter(str) {
+    if (str.length === 0) {
+        return str;
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
---
+++ Problems/toUpperCase.ts
@@ -0,0 +1,9 @@
+function capitalizeFirstLetter(str: string): string {
+
+    if (str.length === 0) {
+        return str;
+    }
+
+
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
